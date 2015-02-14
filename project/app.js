dadf895c771fc08c6c74c895256fdf65d75a566c
Shreyas Balaji <shreyasb99@gmail.com>
2015-02-14 14:23:44 -0600

    basic setup

--- project/app.js
+++ project/app.js
@@ -2,11 +2,19 @@ var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var settings = require('./settings')
+var session = require('express-session')
+var MongoStore = require('connect-mongo')(session);
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var quest = require('./routes/quest')
 
 var app = express();
 
@@ -19,10 +27,37 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// session setup
+app.use(session({
+    secret: settings.cookie_secret,
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      db : settings.dbName,
+      host: settings.dbHost,
+      port: settings.dbPort
+    })
+}));
+
+// flash
+var flash = require('connect-flash');
+app.use(flash());
+app.use(function(req, res, next){
+    res.locals.success_messages = req.flash('success') || [];
+    res.locals.error_messages = req.flash('error') || [];
+    next();
+});
+
+userz = require('./models/userz.js');
+app.use(userz.loginvar());
+
+// controller setup
 app.use('/', routes);
 app.use('/users', users);
+app.use('/quest', quest);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,6 +79,19 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
+var mongoose = require( 'mongoose' );
+var dbURI = settings.dbURI;
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+
+app.listen(settings.port);
 
 // production error handler
 // no stacktraces leaked to user
